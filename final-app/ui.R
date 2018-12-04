04dee0d30a49b47c4547e708e2b8b7e8581b2f39
Di Claire <claire@DimatoMacBook-Pro.local>
2018-12-03 19:11:41 -0800

    Add content in sunburst

--- final-app/ui.R
+++ final-app/ui.R
@@ -11,7 +11,9 @@ shinyUI(
     tabPanel("Cool", 
              sunburstOutput("sun", width = "100%", height = "500px"),
              sidebarPanel(
-               h4("hi")
+               h4("This shows the percentage of each observations in the survey,
+                  from each job position, to what kind of disorder, to gender,
+                  to which country, to state, to disorder frequency.")
              )
              ),
     tabPanel("Corr"),
