ce215a288c80ebab5bf96c6644f1ca94b812cf9c
Di Claire <claire@DimatoMacBook-Pro.local>
2018-12-03 21:48:57 -0800

    Update ui

--- final-app/ui.R
+++ final-app/ui.R
@@ -19,7 +19,10 @@ shinyUI(
     tabPanel("Corr"),
     tabPanel("Ranking",
              sidebarPanel(
-               
+               radioButtons(
+                 "current_past", "Choose to see most common mental illness in OSMI, 2016",
+                 choices = list("Current" = "current_num", "Past" = "past_num"))
+               )
              ),
              mainPanel(
                tabsetPanel(type = "tabs",
@@ -30,5 +33,5 @@ shinyUI(
                )
              )
   )
-  )
 )
+
