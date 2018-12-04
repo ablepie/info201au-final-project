b9c7029d3646e3f4556be0f3e40221dcada7391b
Di Claire <claire@DimatoMacBook-Pro.local>
2018-12-03 21:03:58 -0800

    Merge doubtless mental illness update

--- final-app/sun_helper.R
+++ final-app/sun_helper.R
@@ -1,63 +1,59 @@
 library(data.table)
 library(dplyr)
 
-data <- data.table::fread("../data/survey_2016.csv") 
 
 write_sunburst_data <- function(){
   data <- data.table::fread("../data/survey_2016.csv") 
   
-  #took the data from colomn 49 to 63
-  half_data <- data[,-1:-48]
-  test_data2 <- half_data[, -2:-7]
-  test_data3 <- test_data2[, -4:-5]
-  final_data <- test_data3[, c(2,6,1,3,4,5,7)]
+  # Select the data columns for age, gender, work position, country/state they work in and whether remote work
+  # if self-diagnosed with mental health
+  final_data <- data[, c(56, 62, 49, 50, 57, 60, 61, 63)]
   
-  #rename the header of the data for sunburst graph
-  colnames(final_data) <- c("Age", "Position", "Mental Illness", "Gender", "Country of Resident", "Resident State", "Remotely")
+  # Rename the header of the data for sunburst graph
+  colnames(final_data) <- c("Age", "Position", "Mental Illness", "Maybe Illness", "Gender", 
+                            "Country of Resident", "Resident State", "Remotely")
   
-  test <- gsub("-", " ", final_data$Position)
+  # Merge doubtless mentall illness and doubtful mental illness, both are self diagnosed 
+  final_data <-
+    final_data %>%
+    mutate(`Mental Illness` = paste0(`Mental Illness`, `Maybe Illness`)) %>%
+    filter(`Mental Illness` != "") %>%
+    select(Age, Position, `Mental Illness`, Gender, `Country of Resident`, `Resident State`, Remotely)
+  
   final_data$Position <- gsub("-", " ", final_data$Position)
   
-
-  final_sun <- final_data%>% 
+  # Create path for sunburst
+  final_sun <- 
+    final_data %>% 
     mutate(path = paste(`Position`, `Mental Illness`, Gender, `Country of Resident`,
                         `Resident State`, `Remotely`, sep="-")) %>% 
-    dplyr::select(path, Age)
+    select(path, Age)
   
   write.csv(final_sun, file= "app-data/final_sun.csv", row.names = FALSE)
 }
 
 write_sunburst_data()
 
+# Get sum for the passed disorder type in current self diagonise
+get_current_sum <- function(illness) {
+  has_illness <- str_detect(final_data$`Mental Illness`, illness)
+  return(sum(has_illness))
+}
+# Get sum for the passed certain disorder type in past diagonise
+get_past_sum <- function(illness) {
+  has_illness <- str_detect(data[ ,52], illness)
+  return(has_illness)
+}
+
 write_disorder_data <- function(){
+  # Based on the survey content, summarize all types of disorder
   disorder <- c("Anxiety Disorder", "Mood Disorder", "Stress Response Syndromes", "Substance Use Disorder",
-                                                "Attention Deficit Hyperactivity Disorder", "Pervasive Developmenta", "Personality Disorder",
-                                                "Seasonal Affective Disorder", "Post-traumatic Stress Disorder", "Autism",
-                                                "Traumatic Brain Injury", "Psychotic Disorder", "Eating Disorder")
-
-  data_49 <- data[,49]
-  colnames(data_49) <- "disorder"
-  data_52 <- data[,52]
-  colnames(data_52) <- "disorder"
-  
-  current_num <- c(sum(str_detect(data_49$disorder, "Anxiety Disorder")), sum(str_detect(data_49$disorder,"Mood Disorder")),
-                 sum(str_detect(data_49$disorder, "Stress Response Syndromes")),sum(str_detect(data_49$disorder, "Substance Use Disorder")),
-                 sum(str_detect(data_49$disorder, "Attention Deficit Hyperactivity Disorder")),sum(str_detect(data_49$disorder, "Pervasive Developmenta")),
-                 sum(str_detect(data_49$disorder, "Personality Disorder")), sum(str_detect(data_49$disorder, "Seasonal Affective Disorder")),
-                 sum(str_detect(data_49$disorder, "Post-traumatic Stress Disorder")), sum(str_detect(data_49$disorder, "Autism")),
-                 sum(str_detect(data_49$disorder, "Traumatic Brain Injury")), sum(str_detect(data_49$disorder, "Psychotic Disorder")),
-                 sum(str_detect(data_49$disorder, "Eating Disorder"))
-)
-
-  past_num <- c(sum(str_detect(data_52$disorder, "Anxiety Disorder")), sum(str_detect(data_52$disorder,"Mood Disorder")),
-              sum(str_detect(data_52$disorder, "Stress Response Syndromes")),sum(str_detect(data_52$disorder, "Substance Use Disorder")),
-              sum(str_detect(data_52$disorder, "Attention Deficit Hyperactivity Disorder")),sum(str_detect(data_52$disorder, "Pervasive Developmenta")),
-              sum(str_detect(data_52$disorder, "Personality Disorder")), sum(str_detect(data_52$disorder, "Seasonal Affective Disorder")),
-              sum(str_detect(data_52$disorder, "Post-traumatic Stress Disorder")), sum(str_detect(data_52$disorder, "Autism")),
-              sum(str_detect(data_52$disorder, "Traumatic Brain Injury")), sum(str_detect(data_52$disorder, "Psychotic Disorder")),
-              sum(str_detect(data_52$disorder, "Eating Disorder"))
-)
-
+                "Attention Deficit Hyperactivity Disorder", "Pervasive Developmenta", "Personality Disorder",
+                "Seasonal Affective Disorder", "Post-traumatic Stress Disorder", "Autism",
+                "Traumatic Brain Injury", "Psychotic Disorder", "Eating Disorder", "Obsessive-Compulsive Disorder",
+                "Addictive Disorder", "Other", "Dissociative Disorder", "Psychotic Disorder")
+  current_num <- lapply(paste(disorder, collapse = "|"), get_current_sum)
+  past_num <- get_past_sum(paste(disorder, collapse = "|"))
   result <- data.frame(disorder, current_num, past_num)
   write.csv(result, file= "app-data/result_disorder.csv")
 }
